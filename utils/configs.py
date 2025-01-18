8be7aa5d131e53abeb1b4fe5c7375e48db94ccf2
Diego Melo <diego.nac@hotmail.com>
2025-01-18 17:36:56 -0300

    atualiza configurações de IPs para dispositivos, substitui IPs locais por IPs públicos e organiza as configurações gerais

--- utils/configs.py
+++ utils/configs.py
@@ -1,3 +1,21 @@
+# from .ips import *
+
+# # Configuração de multicast
+# MCAST_GROUP = '239.1.1.1'
+# MCAST_PORT = 7000
+# BUFFER_SIZE = 2048
+# DISCOVERY_DELAY = 2
+# LOCAL_IP = get_local_ip()  # Deve ser o IP da máquina que está rodando
+
+# # Configurações específicas para dispositivos
+# LAMP_IP = generate_device_ip(LOCAL_IP, 10)
+# MOTION_SENSOR_IP = generate_device_ip(LOCAL_IP, 5)
+# DEVICES_PORT = 5006
+
+# # Configurações gerais
+# PROJECT_NAME = "Smart Home"
+
+
 from .ips import *
 
 # Configuração de multicast
@@ -8,8 +26,11 @@ DISCOVERY_DELAY = 2
 LOCAL_IP = get_local_ip()  # Deve ser o IP da máquina que está rodando
 
 # Configurações específicas para dispositivos
-LAMP_IP = generate_device_ip(LOCAL_IP, 10)
-MOTION_SENSOR_IP = generate_device_ip(LOCAL_IP, 5)
+CLIENT_IP = "52.54.249.75"  # IP público da instância do client
+GATEWAY_IP = "18.212.68.69"  # IP público da instância do gateway
+LAMP_IP = "3.83.113.181"  # IP público da instância da lâmpada
+
+# Porta padrão para dispositivos
 DEVICES_PORT = 5006
 
 # Configurações gerais
